7cebf54442cc8b6cd1c15c3180b3d62700748beb
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 08:47:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,17 +2,17 @@
 
 
 def line(people_in_line)
-if people_in_line != [] 
-  new_array = []
-  people_in_line.each_with_index do |person, index|
-    index = index + 1 
-    item = index.to_s + ". " + person
-    
+  if people_in_line != [] 
+    new_array = []
+    people_in_line.each_with_index do |person, index|
+      index = index + 1 
+      item = index.to_s + ". " + person
+      
+    end 
+  else
+    puts {"The line is currently empty."
   end 
-else
-  puts {"The line is currently empty."
-end 
-
+  
 
 end 
 
