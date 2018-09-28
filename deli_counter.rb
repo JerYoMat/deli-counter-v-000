2c2e223723f958a172fcc69a2236bab56691bcff
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 08:42:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,8 @@
 # Write your code here.
 
+
 def line(people_in_line)
-  if people_in_line != []
-   counter = 1 
-    people_in_line.collect do |person|
-      "#{counter}. #{person}"
-      counter += 1 
-    end
-      people_in_line
-   
-  else 
-    puts "The line is currently empty."
-  
-  end 
+
+
 end 
+
