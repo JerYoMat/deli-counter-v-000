76f01a3e5bd1ecf3de0d45dbe53837ab99e1796e
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 08:54:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(people_in_line)
   numbered_list = []
   people_in_line.each_with_index do |person, index|
     numbered_list << "#{index + 1}. #{person}"
-    numbered_list
+ 
   end 
-
+   numbered_list
 end
 
 
