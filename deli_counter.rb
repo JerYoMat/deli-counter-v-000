14581edfb8c2da945061dc50d3ca124758e76ed6
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 08:59:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,13 @@
 
 
 def line(people_in_line)
-
-  numbered_list = []
-  people_in_line.each_with_index do |person, index|
-    numbered_list << "#{index + 1}. #{person}"
- 
+  if people_in_line.size == 0
+    numbered_list = []
+    people_in_line.each_with_index do |person, index|
+      numbered_list << "#{index + 1}. #{person}"
+    end 
+     puts "The line is currently: " + numbered_list.join(" ")
+  else 
+    puts "The line is currently empty."
   end 
-   puts "The line is currently: " + numbered_list.join(" ")
 end
