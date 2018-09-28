512d53da50f9cfaf768cee0910639d249057e72a
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 08:28:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,10 @@
 # Write your code here.
 
 def line(array)
-
-array == [] ? puts "The line is currently empty." : array 
+if array == []
+  puts "The line is currently empty."
+else 
+  array 
+end 
 
 end
