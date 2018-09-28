f29c13984a2d7df00cb311453904c2b6657754df
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 08:46:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(people_in_line)
   new_array = []
   people_in_line.each_with_index do |person, index|
     index = index + 1 
-    item = index + ". " + person
+    item = index.to_s + ". " + person
     
   end 
 
